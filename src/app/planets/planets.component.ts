eb2f5bb36f74370d2c637317de8b86c65d098201
Bl4ky113 <bl4ky@proton.me>
2023-01-26 02:57:37 -0500

    Added planets slideshow, swapi planet service

--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwapiService } from '../services/swapi.service';
 
 @Component({
   selector: 'planets',
   templateUrl: './planets.component.html',
   styleUrls: ['./planets.component.scss']
 })
-export class PlanetsComponent {
+export class PlanetsComponent implements OnInit {
+  constructor (private swapi: SwapiService) {  }
 
+  planetList: Array<any> = [];
+  planetTagList!: HTMLCollectionOf<Element>;
+  isPlanetListComplete: boolean = false;
+
+  currentIndex: number = 0;
+  maxIndex: number = 0;
+
+  planetUrl: string = "";
+
+  ngOnInit () {
+    this.addPlanetList(1);
+  }
+
+  addPlanetList (pageNumber: number) { // Nice Implementation
+    this.swapi.getPlanetList(pageNumber).subscribe(
+      (json: any) => {
+        this.planetList.push(...json.results);
+
+        if (json.next === null) {
+          this.isPlanetListComplete = true;
+          this.maxIndex = this.planetList.length - 1;
+          this.planetTagList = document.getElementsByClassName('info');
+          this.updateSlideShowIndex();
+
+          return null;
+        }
+
+        return this.addPlanetList(pageNumber + 1);
+      }
+    )
+  }
+
+  changeSlideShowIndex (change: number) {
+    if (
+      change > 0 &&
+      this.currentIndex >= this.maxIndex
+    ) { return; }
+    else if (
+      change < 0 &&
+      this.currentIndex <= 0
+    ) { return; }
+
+    this.updateSlideShowIndex();
+    this.currentIndex += change;
+    this.updateSlideShowIndex();
+  }
+
+  updateSlideShowIndex () {
+    this.planetTagList[this.currentIndex].classList.toggle('info--show');
+  }
 }
--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
+  URL_API: string = 'https://swapi.dev/api/';
 
-  constructor() { }
+  constructor (private http: HttpClient) { }
+
+  getPlanetList (pageNumber=1): Observable<object> {
+    return this.http.get(this.URL_API + 'planets/?page=' + pageNumber);
+  }
 }
