a76e8ffd333381960977d7cfa4d96cb8ea376eb3
Bl4ky113 <bl4ky@proton.me>
2023-01-26 01:43:34 -0500

    Added Slideshow and main sections style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { ResidentsComponent } from './residents/residents.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    ResidentsComponent,
+    VehiclesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'planets', component: PlanetsComponent},
+      {path: 'residents/:planet_id', component: ResidentsComponent},
+      {path: 'vehicles/:resident_id', component: VehiclesComponent},
+      {path: '', redirectTo: 'planets', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
